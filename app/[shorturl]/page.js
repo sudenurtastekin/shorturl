9d6096a6cc75e0f7c7451c02b03f48c4eb2b3c23
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:39:18 +0300

    shorturl page.js tamamlandı

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -8,3 +8,16 @@ const ShortUrlPage = async ({ params }) => {
       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0"
     }
   });
+  const data = await response.json();
+
+  if (data.length > 0) {
+    const longUrl = data[0].long_url;
+
+    redirect(longUrl);
+  } else {
+
+    redirect('/404');
+  }
+};
+
+export default ShortUrlPage;
