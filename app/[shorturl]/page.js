bafdec351fc1bc1f93f9a63f30c9152be6ccebc7
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:38:40 +0300

    api key eklendi

---
+++ app/[shorturl]/page.js
@@ -0,0 +1,10 @@
+import { redirect } from 'next/navigation';
+
+const ShortUrlPage = async ({ params }) => {
+  const { shorturl } = params;
+  const response = await fetch(`https://nkmvhnwulrqvvmnzfmdz.supabase.co/rest/v1/urls?short_url=eq.${shorturl}`, {
+    headers: {
+      "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0"
+    }
+  });
