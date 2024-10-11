76b030f09cf02c1570610ba1870770ae070fafcf
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:40:58 +0300

    signup function eklendi

--- app/login/actions.js
+++ app/login/actions.js
@@ -23,3 +23,24 @@ export async function login(formData) {
   revalidatePath('/', 'layout')
   redirect('/')
 }
+
+
+export async function signup(formData) {
+  const supabase = createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
