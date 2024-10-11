66778558cac9cf7851574d5660fefb2dacca8c9b
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 23:28:29 +0300

    fixed

--- app/shortly/components/DesktopHeader.js
+++ app/shortly/components/DesktopHeader.js
@@ -1,9 +1,13 @@
 import Logo from "@/components/svgs/logo";
 import Link from "next/link";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from '@supabase/supabase-js';
+
+
 import { signOut } from "@/actions/auth";
+
+
 export default async function DesktopHeader() {
-  const supabase = createClient();
+  const supabase = createClient('https://nkmvhnwulrqvvmnzfmdz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXZobnd1bHJxdnZtbnpmbWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MDI2NjAsImV4cCI6MjA0MzM3ODY2MH0.57lDrq8eEdkSO_CpN-rSacX6j_cljo2_RA_tlp00Jl0');
   const { data: { user }, error } = await supabase.auth.getUser();
   return (
     <div className="desktopHeader">
