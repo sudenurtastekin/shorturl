598ddeb30b2f32b5342ab4592b2cd27a043ba11c
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:47:54 +0300

    footera başlandı

---
+++ app/shortly/components/Footer.js
@@ -0,0 +1,6 @@
+import FacebookIcon from "@/components/svgs/icon-facebook";
+import TwitterIcon from "@/components/svgs/icon-twitter";
+import PinterestIcon from "@/components/svgs/icon-pinterest";
+import InstagramIcon from "@/components/svgs/icon-instagram";
+
+const Footer = () => {
