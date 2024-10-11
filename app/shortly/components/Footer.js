bb980375c2c8d43c5bc2225b95e34b8e912dcb2c
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:48:18 +0300

    footer item oluşturuldu

--- app/shortly/components/Footer.js
+++ app/shortly/components/Footer.js
@@ -4,3 +4,14 @@ import PinterestIcon from "@/components/svgs/icon-pinterest";
 import InstagramIcon from "@/components/svgs/icon-instagram";
 
 const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>Shortly</h1>
+      <div className="footerItem">
+        <div className="secondCol">
+          <div className="features">
+            <h3>Features</h3>
+            <a href="#">Link Shortening</a>
+            <a href="#">Branded Links</a>
+            <a href="#">Analytics</a>
+          </div>
