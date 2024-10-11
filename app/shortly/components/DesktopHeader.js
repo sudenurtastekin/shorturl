f74e7bf2b16092f6956608485d316c9c68f488ee
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-11 22:46:37 +0300

    desktıp header yapısı oluştu

--- app/shortly/components/DesktopHeader.js
+++ app/shortly/components/DesktopHeader.js
@@ -5,3 +5,31 @@ import { signOut } from "@/actions/auth";
 export default async function DesktopHeader() {
   const supabase = createClient();
   const { data: { user }, error } = await supabase.auth.getUser();
+  return (
+    <div className="desktopHeader">
+      <Logo />
+      <div className="desktopNav">
+        <div className="nav">
+          <a href="#">Features</a>
+          <a href="#">Pricing</a>
+          <a href="#">Resources</a>
+        </div>
+      </div>
+      <div className="loginNav">
+        {user ? (
+          <div className="admin-header">
+            <span>Hoş geldin <em><strong>{user.email}</strong></em></span>
+            <form action={signOut}>
+              <button type="submit" className="cikis">Çıkış Yap</button>
+            </form>
+          </div>
+        ) : (
+          <>
+            <Link href="/login">Login</Link>
+            <Link href="/signUp">Sign Up</Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
